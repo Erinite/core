d3f3469eb52944ccf7fdb1e75cca0d94e5212dae
danielytics <danielytics@users.noreply.github.com>
2019-11-08 03:23:39 +0000

    Add clj-time to dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT"
             :url "https://github.com/Erinite/erinite-core/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-time "0.15.0"]]
   :repl-options {:init-ns erinite.core})
