15d3b5a413e192af56c52b9eea36f631cd8de0cc
danielytics <danielytics@users.noreply.github.com>
2020-07-24 18:51:36 +0100

    Clean up Queue and notification boundaries

--- src/erinite/services/notifications.clj
+++ src/erinite/services/notifications.clj
@@ -2,16 +2,10 @@
   (:require [integrant.core :as ig]))
 
 (defprotocol Notifications
-  (listen! [conn topic-handlers] "Lisen for notifications on a topic")
-  (notify! [conn topic message] "Notify on a topic")
-  (close! [conn listener] "Close a listener"))
+  (notify! [conn topic message] "Notify on a topic"))
 
-(defrecord Boundary [conn]
-  Notifications
-  (listen! [_ topic-handlers] (listen! conn topic-handlers))
-  (notify! [_ topic message] (notify! conn topic message))
-  (close! [_ listener] (close! conn listener)))
+(defrecord Boundary [conn])
 
-(defmethod ig/init-key :erinite.services/notifications [_ notifications]
-  (assert (satisfies? notifications Notifications))
-  (->Boundary notifications))
+(defmethod ig/init-key :erinite.services/notifications
+  [_ conn]
+  (->Boundary conn))
--- src/erinite/services/queue.clj
+++ src/erinite/services/queue.clj
@@ -2,18 +2,10 @@
   (:require [integrant.core :as ig]))
 
 (defprotocol Queue
-  (open! [conn] "Open connection to the queue")
-  (enqueue! [conn message] "Enqueue a message")
-  (listen! [conn callback] "Listen for an dequeue a message")
-  (close! [conn] "Close connection to the queue"))
+  (enqueue! [conn message] "Enqueue a message"))
 
-(defrecord Boundary [conn]
-  Queue
-  (open! [_] (open! conn))
-  (enqueue! [_ message] (enqueue! conn message))
-  (listen! [_ callback] (listen! conn callback))
-  (close! [_] (close! conn)))
+(defrecord Boundary [conn])
 
-(defmethod ig/init-key :erinite.services/queue [_ queue]
-  (assert (satisfies? queue Queue))
-  (->Boundary queue))
+(defmethod ig/init-key :erinite.services/queue
+  [_ conn]
+  (->Boundary conn))
