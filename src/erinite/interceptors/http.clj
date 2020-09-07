39ba649cc812d8105da654063f89c34dc953925f
danielytics <danielytics@users.noreply.github.com>
2020-09-07 20:41:21 +0100

    Make all HTTP responses return a body, so that transit/json parsers on client have a body to parse

--- src/erinite/interceptors/http.clj
+++ src/erinite/interceptors/http.clj
@@ -28,15 +28,19 @@
      `[:ok response]` -- 200, with `response` as body
      `[:created responese]` -- 201, with `response` as body
      `[:accepted response]` -- 202, with `response` as body
+     `[:no-content]` -- 204
      `[:redirect url]` -- 307, with `url` as `Location` header
-     `[:not-found]` -- 404
-     `[:not-authorized]` -- 401
-     `[:forbidden]` -- 403
      `[:user-error]` -- 400
      `[:invalid]` -- 400
+     `[:not-authorized]` -- 401
+     `[:forbidden]` -- 403
+     `[:not-found]` -- 404
+     `[:conflict]` == 409
      `[:too-many-requests]` -- 429
      `[:server-error]` -- 500
+     `[:bad-gateway]` -- 502
      `[:unavailable]` -- 503
+     `[:gateway-timeout]` -- 504
    "
   []
   {:name :translate-responses
@@ -45,6 +49,6 @@
                                 (when-let [{:keys [code body headers]} (get status->codes status)]
                                   {:status code
                                    :headers (when headers (headers data))
-                                   :body (when body (body data))}))]
+                                   :body (if body (body data) {})}))]
               (assoc context :response response)
               context))})
